52c80225301c11fc86f454cec16d3aa5a6cd822b
Arthemyst <pawcio262@gmail.com>
2025-06-14 11:41:43 +0200

    added test_parsing_log_without_time test
    Signed-off-by: Arthemyst <pawcio262@gmail.com>

--- src/tests/test_suspicious_patterns.py
+++ src/tests/test_suspicious_patterns.py
@@ -3,8 +3,15 @@ from datetime import datetime
 from src.suspicious_patterns import parse_time
 
 
-def test_right_time_parsing():
+def test_parsing_log_with_time():
     logs = "2025 Jun 11 11:00:01 server sshd[11111]: Failed password for invalid user admin from 10.0.0.1 port 54321 ssh2"
 
     time_match = parse_time(logs)
     assert (datetime(2025, 6, 11, 11, 0, 1) == time_match)
+
+
+def test_parsing_log_without_time():
+    logs = "No data to parse"
+
+    time_match = parse_time(logs)
+    assert (time_match is None)
