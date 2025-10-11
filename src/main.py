864758da91d42ce93b0ca25b2b2ba0c4493ed8be
arthemyst <pawel.lukmin@gmail.com>
2025-10-11 12:25:25 +0200

    created feature for logs monitoring in real time

--- src/main.py
+++ src/main.py
@@ -3,12 +3,17 @@ import typer
 from logs_anaylyzer import LogsAnalyzer
 
 app = typer.Typer()
-
+paths_to_files = ["./test.log"]
 
 @app.command()
-def analyze_logs(path_to_file: str) -> None:
-    alerts = LogsAnalyzer.analyze_logs(path_to_file)
-    LogsAnalyzer.generate_pdf_report(alerts)
+def analyze_logs(path_to_file: str = None, realtime: bool = False) -> None:
+    if realtime:
+        LogsAnalyzer.start_realtime_monitoring(paths_to_files)
+    elif path_to_file:
+        alerts = LogsAnalyzer.analyze_logs_file(path_to_file)
+        LogsAnalyzer.generate_pdf_report(alerts)
+    else:
+        typer.echo("Please specify --file or --realtime")
 
 
 if __name__ == "__main__":
