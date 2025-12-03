d65d42b7326e7d4000245a7112f684e1cc178d2d
arthemyst <pawel.lukmin@gmail.com>
2025-12-03 16:41:53 +0100

    created SyslogTCPClientHandler class

--- src/syslog_receiver.py
+++ src/syslog_receiver.py
@@ -30,3 +30,33 @@ class SyslogUDPProtocol(asyncio.DatagramProtocol):
     def datagram_received(self, data, addr):
         message = data.decode(errors="ignore")
         asyncio.create_task(process_syslog_message(message, "UDP"))
+
+class SyslogTCPClientHandler(asyncio.Protocol):
+    def __init__(self):
+        self.buffer = b""
+
+    def data_received(self, data: bytes):
+        self.buffer += data
+
+        while True:
+            if b" " not in self.buffer:
+                return
+
+            try:
+                length_str, rest = self.buffer.split(b" ", 1)
+                if not length_str.isdigit():
+                    logger.warning(f"[TCP] Invalid length prefix")
+                    self.buffer = b""
+                    return
+
+                msg_len = int(length_str)
+            except ValueError:
+                return
+
+            if len(rest) < msg_len:
+                return
+
+            raw_msg = rest[:msg_len].decode(errors="ignore")
+            self.buffer = rest[msg_len:]
+
+            asyncio.create_task(process_syslog_message(raw_msg, "TCP"))
