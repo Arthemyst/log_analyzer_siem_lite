426a67fe6ff98daf7c47d72e26e2f4e9b78ad276
arthemyst <pawel.lukmin@gmail.com>
2025-12-16 22:51:18 +0100

    added _enrich_with_mitre function

--- src/ids/detection_engine.py
+++ src/ids/detection_engine.py
@@ -1,7 +1,15 @@
+from mitre_mapping import MITRE_MAP
+
 class DetectionEngine:
     def __init__(self, ml_detector, scan_detector):
         self.ml = ml_detector
         self.scan = scan_detector
+
+    def _enrich_with_mitre(self, alert: dict) -> dict:
+        mapping = MITRE_MAP.get(alert["type"])
+        if mapping:
+            alert["mitre"] = mapping
+        return alert
 
     def process_flow(self, flow: dict):
         alerts = []
